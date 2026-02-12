0eb78f6700fb3587aeb99c644822f6a3401e31c6
wotex <efe.fishekqiu@elev.ga.lbs.se>
2026-02-12 14:10:00 +0100

    gamepage done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let games = {
         label: "Streets n Heists",
         thumbnail: "/assets/gamepics/snh1.png",
         video: "/assets/videos/streetsnheistgameplay.mp4",
+        logo: "./assets/logos/streetsnheistlogo.png",
         supportedPlatforms: {
             ["pc"]: false,
             ["ps4"]: true,
@@ -23,11 +24,50 @@ let games = {
             ["Tillgängligt"]: "26-02-27",
             ["Marknadsföringsinnehåll"]: "Ja",            
         },
+        tags: {
+            ["SHOOTING"]: true,
+            ["2D"]: true,
+        },
+        description: "Kliv in i en öppen stad inspirerad av klassiska top down actionspel där tempot är högt och faran lurar runt varje hörn Du tar rollen som en orädd karaktär som försöker överleva i en brutal stad fylld av brott kaos och ständiga polisinsatser Utforska gatorna ta dig an uppdrag samla vapen och fly från polisen i intensiva biljakter och explosiva konfrontationer Ju mer uppmärksamhet du väcker desto hårdare blir jakten Kan du hålla dig steget före lagen och skapa ditt eget namn i stadens undre värld",
+        faq: `
+            <p><strong>What is Street N Heists?</strong></p>
+            <br>
+            <p>Street N Heists is a fast-paced top-down action game developed and published by Interactive. Set in a crime-filled open city, players take on the role of an ambitious outlaw completing dangerous jobs, escaping intense police chases, and building their reputation in the underground world.</p>
+            <br>
+            <p><strong>When will Street N Heists be released?</strong></p>
+            <br>
+            <p>Street N Heists is scheduled for release in 2026. The exact launch date will be announced soon on our official channels.</p>
+            <br>
+            <p><strong>On which platforms will Street N Heists be available?</strong></p>
+            <br>
+            <p>Street N Heists will be available on PC at launch. Additional platform releases may be announced at a later date.</p>
+            <br>
+            <p><strong>Does Street N Heists support controller input?</strong></p>
+            <br>
+            <p>Yes. Street N Heists supports both keyboard and mouse as well as compatible controllers on PC.</p>
+            <br>
+            <p><strong>Is there multiplayer in Street N Heists?</strong></p>
+            <br>
+            <p>At launch, Street N Heists is designed as a single-player experience. Future updates may expand gameplay features.</p>
+            <br>
+            <p><strong>Will there be post-launch content?</strong></p>
+            <br>
+            <p>Interactive plans to support Street N Heists with updates, gameplay improvements, and additional content after release. More details will be shared closer to launch.</p>
+            <br>
+            <p><strong>Where can I purchase Street N Heists?</strong></p>
+            <br>
+            <p>Street N Heists will be available through major digital PC storefronts. More information will be provided prior to release.</p>
+            <br>
+            <p><strong>Where can I follow updates about the game?</strong></p>
+            <br>
+            <p>You can follow the latest news and announcements from Interactive on our official website and social media channels.</p>
+        `,
     },
     ["parryvsgod"]: {
         label: "Parry Vs God",
         thumbnail: "/assets/gamepics/pvg1.png",
         video: "/assets/videos/parryvsgodgameplay.mp4",
+        logo: "./assets/logos/parryvsgodlogo.png",
         supportedPlatforms: {
             ["pc"]: true,
             ["ps4"]: true,
@@ -44,10 +84,48 @@ let games = {
         },
         depends: {
             ["Platform"]: "Windows 10/11",
-            ["Utvecklare"]: "Kosta/Adis/Leon",
-            ["Tillgängligt"]: "26-02-27",
+            ["Utvecklare"]: "Missam/Sigge",
+            ["Tillgängligt"]: "25-02-27",
             ["Marknadsföringsinnehåll"]: "Ja",            
         },
+        tags: {
+            ["OPEN WORLD"]: true,
+            ["3D"]: true,
+        },
+        description: "Kliv in i en episk öppen värld där endast de modigaste krigarna överlever Varje boss som dyker upp är en unik utmaning som kräver skicklighet snabbhet och strategi Som spelare tar du rollen som en mästare i parry och attack, redo att möta fruktansvärda gudar i intensiva strider Utforska världen, upptäck hemliga platser och förbered dig på svåra konfrontationer Ju fler bossar du besegrar desto mäktigare blir du Kan du bemästra varje gud och bli den ultimata krigaren i Parry Vs God",
+        faq: `
+            <p><strong>What is Parry Vs God?</strong></p>
+            <br>
+            <p>Parry Vs God is a fast-paced boss battle game set in a vast open world. Players take on the role of a master fighter specializing in parry and attack, facing epic gods in challenging duels to prove their skill and strategy.</p>
+            <br>
+            <p>When will Parry Vs God be released?</strong></p>
+            <br>
+            <p>Parry Vs God is scheduled for release in 2026. The exact launch date will be announced soon on our official channels.</p>
+            <br>
+            <p><strong>On which platforms will Parry Vs God be available?</strong></p>
+            <br>
+            <p>Parry Vs God will be available on PC at launch. Console releases may be announced at a later date.</p>
+            <br>
+            <p><strong>Does Parry Vs God support controller input?</strong></p>
+            <br>
+            <p>Yes. Parry Vs God supports both keyboard and mouse as well as compatible controllers on PC.</p>
+            <br>
+            <p><strong>Is there multiplayer in Parry Vs God?</strong></p>
+            <br>
+            <p>At launch, Parry Vs God is designed as a single-player experience focused entirely on boss battles. Future updates may expand gameplay features.</p>
+            <br>
+            <p><strong>Will there be post-launch content?</strong></p>
+            <br>
+            <p>Interactive plans to support Parry Vs God with updates, additional bosses, gameplay improvements, and new open world areas after release.</p>
+            <br>
+            <p><strong>Where can I purchase Parry Vs God?</strong></p>
+            <br>
+            <p>Parry Vs God will be available through major digital PC storefronts. More information will be provided prior to release.</p>
+            <br>
+            <p><strong>Where can I follow updates about the game?</strong></p>
+            <br>
+            <p>You can follow the latest news and announcements from Interactive on our official website and social media channels.</p>
+        `,        
     },
 }
 
@@ -170,7 +248,7 @@ $(document).ready(function () {
     logoReload()
     $('.video')[0].play(); 
     selectGame("streetnheists")
-    openGamePage("streetnheists")
+    // openGamePage("streetnheists")
 });
 
 
@@ -187,25 +265,36 @@ let currentOpenedGame = "null";
 function openGamePage(id) {
     if (games[id]) {
         currentOpenedGame = id;
-        $("body").css("overflow", "hidden")
+        $("body").css("overflow-y", "hidden")
+        selectGamepageNavbar("oversikt")
+        $("header").hide()
         let data = games[id];
         $(".gamepage-contentScreen>video").hide()
+        $(".gamepage-rightHandSide-logo").attr("src", data.logo)
         $(".gamepage-contentScreen>img").attr("src", data.thumbnail)
-
         $(".gamepage-footerWrapper-contents").html("")
         $.each(data.contents, function(k, v) {
             let content = `<img src="${k}" alt="${k} content">`
             if (isUrlVideo(k) == true) {
                 content = `<video class="as" src="${k}" muted playsinline paused preload="metadata"></video>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"></path></svg>`
+                <svg class="startVideoShowcase" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"></path></svg>
+                <svg class="pauseVideoShowcase" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M96 448h106.7V64H96v384zM309.3 64v384H416V64H309.3z"></path></svg>
+                `
             }
-            
             $(".gamepage-footerWrapper-contents").append(`
                 <div class="gamepage-footerWrapper-contents-box" data-id="${v}">
                     ${content}
                 </div>
             `)
         })
+        $(".gamepage-tags-wrapper").html("")
+        $.each(data.tags, function(k, v) {
+            $(".gamepage-tags-wrapper").append(`
+                <div class="gamepage-tags-wrapper-tag">${k}</div>
+            `)
+        })
+        $(".gamepage-description").html(data.description)
+        $(".gamepage-faq").html(data.faq)
         $(".gamepage-dependsWrapper").html("")
         $.each(data.depends, function(k, v) {
             $(".gamepage-dependsWrapper").append(`
@@ -215,36 +304,121 @@ function openGamePage(id) {
                 </div>    
             `)
         })
+        $(".gamepage-dependsWrapper").append(`
+            <div class="gamepage-dependsWrapper-box">
+                <button class="opacityLow"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z"></path></svg>Ladda ner</button>
+            </div>    
+            <div class="gamepage-dependsWrapper-box">
+                <button style="background-color: #343437; color: #fff;" class="opacityLow"><svg style="font-size: 13px;" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M64 32C64 14.3 49.7 0 32 0S0 14.3 0 32L0 64 0 368 0 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-128 64.3-16.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30l0-247.7c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L64 48l0-16z"></path></svg>Anmäl</button>
+            </div>    
+        `)
         setTimeout(() => {
             $('.as')[0].pause(); 
         }, 100);
         selectGamePageContent(1)
+        $(".gamepage-wrapper").show()
     }
 }
+
+function selectGamepageNavbar(type) {
+    $(".gamepage-navbar-button").removeClass("gamepage-navbar-button-selected")
+    $(`.gamepage-navbar-button[data-type="${type}"]`).addClass("gamepage-navbar-button-selected")
+
+    if (type == "faq") {
+        $(".gamepage-rightHandSide, .gamepage-tags, .gamepage-description, .gamepage-showcaseProfile").hide()
+        $(".gamepage-faq").show()
+    } else {
+        $(".gamepage-faq").hide()
+        $(".gamepage-rightHandSide, .gamepage-tags, .gamepage-description, .gamepage-showcaseProfile").show()
+    }
+}
+
+$(document).on("click", ".gamepage-navbar-button", function () {
+    selectGamepageNavbar($(this).attr("data-type"))
+})
 
 $(document).on("click", ".gamepage-footerWrapper-contents-box", function () {
     selectGamePageContent($(this).attr("data-id"))
 })
 
+$(document).on("click", ".selected-game", function () {
+    openGamePage(currentGame)
+})
+
+$(document).on("click", ".gamepage-wrapper-close", function () {
+    currentOpenedGame = "null";
+    $("body").css("overflow-y", "auto")
+    selectGamepageNavbar("oversikt")
+    $("header").show()
+    $(".gamepage-wrapper").hide()
+})
+
 function selectGamePageContent(id) {
+
     if (games[currentOpenedGame]) {
-        $(".gamepage-footerWrapper-contents-box").removeClass("gamepage-footerWrapper-contents-box-selected")
-        $.each(games[currentOpenedGame].contents, function(k, v) {
+
+        const $video = $('.gamepage-contentScreen>video');
+        const videoEl = $video[0];
+
+        $(".gamepage-footerWrapper-contents-box")
+            .removeClass("gamepage-footerWrapper-contents-box-selected");
+
+        $(".pauseVideoShowcase").hide();
+        $(".startVideoShowcase").show();
+
+        $.each(games[currentOpenedGame].contents, function (k, v) {
+
             if (v == id) {
+
                 if (isUrlVideo(k) == true) {
-                    $(".gamepage-contentScreen>img").hide()
-                    $(".gamepage-contentScreen>video").attr("src", k).show()
-                    $('.gamepage-contentScreen>video')[0].play(); 
+
+                    // Eski listenerları temizle
+                    $video.off("play pause ended");
+
+                    $(".gamepage-contentScreen>img").hide();
+                    $video.attr("src", k).show();
+
+                    // 🎯 PLAY olduğunda icon değiştir
+                    $video.on("play", function () {
+                        $(`.gamepage-footerWrapper-contents-box[data-id="${id}"]`)
+                            .find(".startVideoShowcase").hide()
+                            .end()
+                            .find(".pauseVideoShowcase").show();
+                    });
+
+                    // 🎯 PAUSE veya ENDED olduğunda resetle
+                    $video.on("pause ended", function () {
+                        $(".pauseVideoShowcase").hide();
+                        $(".startVideoShowcase").show();
+                    });
+
+                    videoEl.play();
+
                 } else {
-                    $(".gamepage-contentScreen>video").hide()
-                    $(".gamepage-contentScreen>img").attr("src", k).show()
+
+                    if (videoEl) {
+                        videoEl.pause();
+                        videoEl.currentTime = 0;
+                    }
+
+                    $video.hide();
+                    $(".gamepage-contentScreen>img")
+                        .attr("src", k)
+                        .show();
                 }
-                $(`.gamepage-footerWrapper-contents-box[data-id="${id}"]`).addClass("gamepage-footerWrapper-contents-box-selected")
-                return true
+
+                $(`.gamepage-footerWrapper-contents-box[data-id="${id}"]`)
+                    .addClass("gamepage-footerWrapper-contents-box-selected");
+
+                return false;
             }
-        })
+
+        });
+
     }
+
 }
+
 
 function smoothHorizontalScroll(element, distance) {
     const start = element.scrollLeft;
